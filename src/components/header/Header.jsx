d8d59e9e4a94673882305ad40f2bd747058fd296
Andrey <kotlyar.andrey03@mail.ru>
2025-03-27 21:09:26 +0300

    Застилизовал header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,41 @@
 import styles from './header.module.css';
+import logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export const Header = () => {
+	const [isButtonActive, setIsButtonActive] = useState(1); //Если равен 1 то класс активности первой кнопке, если 2 то второй
+
 	return (
-		<div className={styles['header']}>
-			<h2>Шапка</h2>
-		</div>
+		<header className={styles['header']}>
+			<div className={styles['logo__buttons']}>
+				<img src={logo} alt="logo" />
+				<Link
+					to="/"
+					onClick={() => setIsButtonActive(1)}
+					className={
+						isButtonActive === 1
+							? `${styles['button__navigate']} ${styles['active']}`
+							: `${styles['button__navigate']}`
+					}
+				>
+					Главная
+				</Link>
+				<Link
+					to="/history"
+					onClick={() => setIsButtonActive(2)}
+					className={
+						isButtonActive === 2
+							? `${styles['button__navigate']} ${styles['active']}`
+							: `${styles['button__navigate']}`
+					}
+				>
+					Вся история
+				</Link>
+			</div>
+			<div className={styles['user']}>
+				<h2>Andrey</h2>
+			</div>
+		</header>
 	);
 };
